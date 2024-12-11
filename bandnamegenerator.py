683771e55ef326e6a3d447557e0a7b44c3f75453
Ihsan <mortalihsan@gmail.com>
2024-12-11 19:29:09 +0500

    Add files via upload

---
+++ bandnamegenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator...")
+City_Name = input("Enter the name of the city you grew up in: ")
+pet_name  = input("Enter your pet's name: ") 
+Band_Name = City_Name + pet_name
+print(Band_Name, "is your band's name")
+print("BYE BYE") 
---
+++ variable practice.py
@@ -0,0 +1,22 @@
+# print and take string input using variable.
+name = input("What is your name?")
+print(name) 
+
+#print and take numeric input: 
+number = input("Enter a Number: ")
+print (number) 
+
+a = 20
+print (a)
+b = 30 
+print (b)
+# changing the values of a and b using a third variable.
+c = a
+a = b
+b = c
+
+# print the new values of a and b. 
+print ("The value of a is: " , a )
+print ("The value of b is: " , b )
+
+
