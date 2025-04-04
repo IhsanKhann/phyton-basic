a4f0f67457bf8c71b3acd2eaabcc775885566688
Ihsan <mortalihsan@gmail.com>
2025-04-04 12:53:49 +0500

    Raise an Exception

---
+++ practice/Exception-Handling.py
@@ -0,0 +1,43 @@
+#Exception
+#Types of Exception and handling exceptions
+#Keywords:
+  #else, finally, try, except, raise
+#custom Exceptions
+#multiple Exceptions
+
+#A custom exception:
+#Raise a Exception:
+def check_Positive_Number(number):
+  if number < 0:
+    raise ValueError("Number not Positive.")
+  return num
+
+class InvalidAgeError(Exception):
+  def __init__(self,message):
+    self.message = message
+    super().__init__(message)
+
+def CheckAge(age):
+  if age < 18:
+    try:
+      InvalidAgeError("Age below 18")
+    except InvalidAgeError as e:
+      print(e)
+    return False
+
+  return True
+
+Age = int(input("Enter Age:"))
+Verification = CheckAge(Age)
+
+if(Verification):
+  print("Your Allowed")
+else:
+  print("Your not Allowed") 
+
+  
+  try:
+    print(check_Positive_Number(-5))
+  except Exception as e:
+    print(e)
+  
